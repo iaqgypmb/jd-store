3c4185934c748fc5c8fdc17af6cd32491b14651c
chenxiyuan801 <chenxiyuan801@gmail.com>
2017-05-18 16:55:36 +0800

    adjust view and controller of login and logout

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,10 +1,18 @@
 class User::SessionsController < Devise::RegistrationsController
 
   def new
-
+    super
   end
 
   def create
-    
+    super
   end
+
+  def destroy
+  super
+  cookies.delete :user_uuid
+  flash[:notice] = "退出成功"
+  redirect_to root_path
+end
+
 end
