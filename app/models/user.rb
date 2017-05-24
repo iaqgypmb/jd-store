0d032e1d7171782ae76d3db8d0293e5948a9263c
chenxiyuan801 <chenxiyuan801@gmail.com>
2017-05-24 15:07:35 +0800

    delete later

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,62 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  has_many :orders
+
+  def admin?
+    is_admin
+  end
+
+  attr_accessor :email, :password, :password_confirmation, :token
+
+  CELLPHONE_RE = /\A(\+86|86)?1\d{10}\z/
+  EMAIL_RE = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
+
+  validates_presence_of :password, message: "密码不能为空",
+    if: :need_validate_password
+  validates_presence_of :password_confirmation, message: "密码确认不能为空",
+    if: :need_validate_password
+  validates_confirmation_of :password, message: "密码不一致",
+    if: :need_validate_password
+  validates_length_of :password, message: "密码最短为6位", minimum: 6,
+    if: :need_validate_password
+
+  # validate :validate_email_or_cellphone, on: :create
+
+  private
+  def need_validate_password
+    self.new_record? ||
+      (!self.password.nil? || !self.password_confirmation.nil?)
+  end
+
+  # # 需要添加邮箱和手机号不能重复的校验
+  #   def validate_email_or_cellphone
+  #     if [self.email, self.cellphone].all? { |attr| attr.nil? }
+  #       self.errors.add :base, "邮箱和手机号其中之一不能为空"
+  #       return false
+  #     else
+  #       if self.cellphone.nil?
+  #         if self.email.blank?
+  #           self.errors.add :email, "邮箱不能为空"
+  #           return false
+  #         else
+  #           unless self.email =~ EMAIL_RE
+  #             self.errors.add :email, "邮箱格式不正确"
+  #             return false
+  #           end
+  #         end
+  #       else
+  #         unless self.cellphone =~ CELLPHONE_RE
+  #           self.errors.add :cellphone, "手机号格式不正确"
+  #           return false
+  #         end
+  #
+  #         unless VerifyToken.available.find_by(cellphone: self.cellphone, token: self.token)
+  #           self.errors.add :cellphone, "手机验证码不正确或者已过期"
+  #           return false
+  #         end
+  #       end
+  #     end
+  #
+  #     return true
+  #   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 
+  # 增加注册登录退出路径
+  resources :users
+  resources :sessions
+  delete '/logout' => 'sessions#destroy', as: :logout
+
   namespace :admin do
     resources :products
     resources :orders do
